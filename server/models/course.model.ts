a90f73403636dd07c32180aa7b75e4abe9054df4
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-02-25 13:52:00 -0500

    Añadido soporte para reseñas y enlaces en el modelo de datos de cursos, mejorando la estructura de comentarios y respuestas.

--- server/models/course.model.ts
+++ server/models/course.model.ts
@@ -1,23 +1,35 @@
 import mongoose, { Document, Model, Schema } from 'mongoose';
 
+/**
+ * Representa un comentario con respuestas opcionales.
+ */
 interface IComment extends Document {
-    user: object,
+    user: object;
     comment: string;
     commentReplices?: IComment[];
 }
 
+/**
+ * Representa una reseña con calificación y comentarios.
+ */
 interface IReview extends Document {
-    user: string,
-    rating: number,
+    user: string;
+    rating: number;
     comment: string;
     commentReplices: IComment[];
 }
 
+/**
+ * Representa un enlace con título y URL.
+ */
 interface ILink extends Document {
     title: string;
     url: string;
 }
 
+/**
+ * Representa los datos de un curso, incluyendo videos, preguntas y sugerencias.
+ */
 interface ICourseData extends Document {
     title: string;
     description: string;
@@ -27,47 +39,51 @@ interface ICourseData extends Document {
     videoLength: number;
     videoPlayer: string;
     links: ILink[];
-    suggestion : string;
+    suggestion: string;
     questions: IComment[];
 }
 
+/**
+ * Representa un curso con su información principal.
+ */
 interface ICourse extends Document {
     name: string;
     description: string;
-    price?: number;
-    estimatedPrice?: number;
+    price?: number; // Precio del curso (opcional)
+    estimatedPrice?: number;  // Precio estimado (en caso de descuentos o variaciones)
     thumbnail: object;
     tags: string;
     level: string;
     demoUrl: string;
-    benefits: {title: string}[];
-    prerequisites: {title: string}[];
+    benefits: { title: string }[];
+    prerequisites: { title: string }[];
     reviews: IReview[];
     courseData: ICourseData;
     ratings?: number;
     purchased: number;
 }
 
-const reviewShema = new Schema<IReview>({
+// Esquema para las reseñas del curso
+const reviewSchema = new Schema<IReview>({
     user: Object,
-    rating: {
-        type: Number,
-        default: 0,
-    },
+    rating: { type: Number, default: 0 },
     comment: String,
 });
 
+// Esquema para los enlaces del curso
 const linkSchema = new Schema<ILink>({
     title: String,
     url: String,
 });
 
+// Esquema para los comentarios y sus respuestas
 const commentSchema = new Schema<IComment>({
     user: Object,
     comment: String,
     commentReplices: [Object],
 });
 
+// Esquema para los datos del curso
 const courseDataSchema = new Schema<ICourseData>({
     videoUrl: String,
     videoThumbnail: Object,
