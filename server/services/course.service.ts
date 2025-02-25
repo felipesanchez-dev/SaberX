4f4d3609b3ca504eab92cde6a2d70107eb2e4430
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-02-25 14:24:35 -0500

    Añadida la funcionalidad para crear un curso en el servicio de cursos.

---
+++ server/services/course.service.ts
@@ -0,0 +1,16 @@
+import { Response } from 'express';
+import CourseModel from '../models/course.model';
+import { CatchAsyncError } from '../middleware/catchAsyncError';
+
+// Crear un curso
+export const createCourse = CatchAsyncError(async (
+    data: any,
+    res: Response
+)=> {
+    const course = await CourseModel.create(data);
+    res.status(201).json({
+        message: 'Curso creado exitosamente',
+        success: true,
+        course,
+    });
+});
