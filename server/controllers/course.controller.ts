9448d65505242c16f15e8219e68744f5290a609f
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-02-25 18:04:59 -0500

    mejorado el controlador para subir y crear un curso, añadiendo manejo de errores y comentarios explicativos

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -4,7 +4,7 @@ import ErrorHandler from '../utils/ErrorHandler';
 import cloudinary from 'cloudinary'
 import { createCourse } from '../services/course.service';
 
-// Actualizar curso
+// Controlador para subir y crear un curso
 export const uploadCourse = CatchAsyncError(async (
     req: Request,
     res: Response,
@@ -13,17 +13,21 @@ export const uploadCourse = CatchAsyncError(async (
     try{
         const data = req.body;
         const thumbnail = data.thumbnail
+            // Si se proporciona una miniatura, se sube a Cloudinary
         if(thumbnail) {
             const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
                 folder: "courses"
             });
+            // Se guarda la información del thumbnail en el curso
             data.thumbnail = {
                 public_id: myCloud.public_id,
                 url: myCloud.secure_url,
             }
         }
+        // Llamado al servicio para crear el curso con los datos procesados
         createCourse(data,res,next);
 
+    // Manejo de errores en caso de algun fallo
     } catch (error: any){
         return next(new ErrorHandler(error.message, 500));
     }
