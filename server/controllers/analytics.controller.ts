5931012e549b4abd8750e3888cf8589b000c0cf1
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-02-27 16:38:09 -0500

    Añadidos controladores y rutas para obtener análisis de cursos y órdenes, incluyendo la gestión de errores. - Se ha actualizado el modelo de curso para incluir timestamps.

--- server/controllers/analytics.controller.ts
+++ server/controllers/analytics.controller.ts
@@ -3,6 +3,8 @@ import { CatchAsyncError } from '../middleware/catchAsyncError';
 import ErrorHandler from '../utils/ErrorHandler';
 import { generateLast12MothsData } from '../utils/analytics.generator';
 import userModel from '../models/user.model';
+import CourseModel from '../models/course.model';
+import OrderModel from '../models/order.Model';
 
 export const getUsersAnalytics = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -11,6 +13,34 @@ export const getUsersAnalytics = CatchAsyncError(async (req: Request, res: Respo
             message: 'Analisis de usuarios por mes obtenido exitosamente',
             success: true,
             users,
+        });
+
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 400));
+    }
+});
+
+export const getCoursesAnalytics = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const courses = await generateLast12MothsData(CourseModel);
+        res.status(200).json({
+            message: 'Analisis de coursos por mes obtenido exitosamente',
+            success: true,
+            courses,
+        });
+
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 400));
+    }
+});
+
+export const getOrderAnalytics = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const orders = await generateLast12MothsData(OrderModel);
+        res.status(200).json({
+            message: 'Analisis de ordenes por mes obtenido exitosamente',
+            success: true,
+            orders,
         });
 
     } catch (error: any) {
--- server/routes/analytics.route.ts
+++ server/routes/analytics.route.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 import { isAuthenticated, authorizeRoles } from '../middleware/auth';
-import { getUsersAnalytics } from '../controllers/analytics.controller';
+import { getCoursesAnalytics, getOrderAnalytics, getUsersAnalytics } from '../controllers/analytics.controller';
 
 const analyticsRouter = express.Router();
 
 analyticsRouter.get('/get-users-analytics', isAuthenticated, authorizeRoles('admin'), getUsersAnalytics);
+
+analyticsRouter.get('/get-courses-analytics', isAuthenticated, authorizeRoles('admin'), getCoursesAnalytics);
+
+analyticsRouter.get('/get-orders-analytics', isAuthenticated, authorizeRoles('admin'), getOrderAnalytics);
+
+
 
 export default analyticsRouter;
