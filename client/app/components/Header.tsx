b96f3804242c0379ccaca8150d9f3856299f7401
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-02-28 03:07:41 -0500

    Agregar enlace al logo en el componente Header

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Link from "next/link";
 import React, { FC, useState } from "react";
 
 type Props = {
@@ -29,7 +30,18 @@ const Header: FC<Props> = ({ open, setOpen, activeItem }) => {
                         ? 'dark:bg-opacity-50 dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border-[#ffffff1c] shadow-xl transition duration-500'
                         : 'w-full border-b dark:border-[#ffffff1c] h-[80px] z-[80] dark:shadow'
                 }`}
-            ></div>
+            >
+                <div className='w-[95%] 800px:w-[92%] m-auto py-2 h-full'>
+                    <div className='w-full h-[80px] flex items-center justify-between p-3'>
+                        <div>
+                            <Link href={'/'}
+                            className={'text-[25px] font-Poppins font-[500] text-black dark:text-white'}>
+                                saberX
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
