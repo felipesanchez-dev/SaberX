3fada3b8f07b7bcf4acaf71ac2cf3b707b923f78
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-02-28 16:30:16 -0500

    Agregar estado para la gestión de la barra de navegación en el componente Header

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -1,4 +1,5 @@
-import React, {FC} from 'react'
+'use client'
+import React, {FC, useState} from 'react'
 
 type Props = {
     open: boolean,
@@ -7,8 +8,18 @@ type Props = {
 }
 
 const Header:FC<Props> = (props) => {
+    const [active, setActive] = useState(false);
+    const [opneSidebar, setOpneSidebar] = useState(false);
     return (
-        <div>Hola</div>
+        <div className='w-full relative'>
+            {/* <div
+                className={`${
+                    active ? 'dark:bg-opacity-50 dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border-[#ffffff1c] shadow-xl transition duration-500'
+                        : 'w-full border-b dark:border-[#ffffff1c] h-[80px] z-[80] dark:shadow'
+                }`}
+            >
+            </div> */}
+        </div>
     );
 };
 
