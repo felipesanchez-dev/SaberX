74c4ac4118403babeda1e017d5874cf7d66544ef
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-02-28 02:48:30 -0500

    Agregar componente Header y optimizar el layout con soporte para estado de desplazamiento

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,18 +1,17 @@
 import "./globals.css";
-import { Poppins } from "next/font/google";
-import { Josefin_Sans } from "next/font/google";
+import { Poppins, Josefin_Sans } from "next/font/google";
 import { ThemeProvider } from "./utils/theme-provider";
 
 const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-  variable: '--font-Poppins'
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-Poppins",
 });
 
 const josefin = Josefin_Sans({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-  variable: '--font-Josefin'
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-Josefin",
 });
 
 export default function RootLayout({
@@ -21,9 +20,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html>
-      <body className={`${poppins.variable} ${josefin.variable} !bg-white dark:bg-gradient-to-b dark:from-gray-900 dark:to-black duration-300`}>
-        <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+    <html lang="es">
+      <body className={`${poppins.variable} ${josefin.variable} !bg-white bg-no-repeat dark:bg-gradient-to-b dark:from-gray-900 dark:to-black duration-300`}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
         </ThemeProvider>
       </body>
