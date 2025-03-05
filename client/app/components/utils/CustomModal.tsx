25a4e32da2e2f75abf3709da3fb6d2ea2fd76bbf
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-03-05 00:11:33 -0500

    Eliminar el título del modal en CustomModal para simplificar la interfaz

--- client/app/components/utils/CustomModal.tsx
+++ client/app/components/utils/CustomModal.tsx
@@ -26,7 +26,6 @@ const CustomModal: FC<Props> = ({
       aria-describedby="modal-modal-description"
     >
       <Box className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-[450px] bg-white dark:bg-slate-900 rounded-[8px] shadow p-4 outline-none">
-        <h2 className="text-center text-lg font-bold">Contenido del Modal</h2>
         <Component setOpen={setOpen} setRoute={setRoute} />
       </Box>
     </Modal>
