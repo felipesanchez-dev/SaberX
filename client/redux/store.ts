a8a0bcf92dc6f4e36d0d263660c6c45aa8232d28
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-03-05 21:24:59 -0500

    Agregar endpoint para refrescar token y inicializar la aplicación al cargar

--- client/redux/store.ts
+++ client/redux/store.ts
@@ -12,3 +12,10 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
 });
+
+const initiaLizeApp = async () => {
+  await store.dispatch(
+    apiSlice.endpoints.refreshToken.initiate({}, { forceRefetch: true })
+  );
+};
+initiaLizeApp();
