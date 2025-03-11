8889dcdf65776ff4705808d134f551731d03fd55
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-03-11 18:09:19 -0500

    Agregar manejo de estado y formulario en el componente CourseContent

--- client/app/components/Admin/Course/CourseContent.tsx
+++ client/app/components/Admin/Course/CourseContent.tsx
@@ -1,11 +1,11 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 
 type Props = {
   active: number;
   setActive: (active: number) => void;
   courseContentData: any;
   setCourseContentData: (data: any) => void;
-  handleSubmit: () => void;
+  handleSubmit: any;
 };
 
 const CourseContent: FC<Props> = ({
@@ -15,7 +15,20 @@ const CourseContent: FC<Props> = ({
   setCourseContentData,
   handleSubmit: handleCourseSubmit,
 }) => {
-  return <div>CourseContent</div>;
+  const [isCollapsed, setIsCollapsed] = useState(
+    Array(courseContentData.length).fill(false)
+  );
+  const [activeSection, setActiveSection] = useState(1);
+
+  const handleSumit = (e: any) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="w-[80%] m-auto mt-24 p3">
+      <form onSubmit={handleSumit}></form>
+    </div>
+  );
 };
 
 export default CourseContent;
