19458fdf367e9ef4e2185517c6be55565aa60e8c
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-03-18 09:17:54 -0500

    Eliminar comentarios innecesarios y mejorar la carga del componente Loader en RootLayout

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -8,7 +8,6 @@ import { Toaster } from "react-hot-toast";
 import { Providers } from "./Provider";
 import { SessionProvider } from "next-auth/react";
 
-// Carga dinámica del Loader para evitar hydration error
 const Loader = dynamic(() => import("./components/Loader/Loader"), {
   ssr: false,
 });
@@ -34,13 +33,12 @@ export default function RootLayout({
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Solo accede a window después de que el cliente monte
     setTheme(
       window.matchMedia("(prefers-color-scheme: dark)").matches
         ? "dark"
         : "light"
     );
-    setLoading(false); // Detiene el loader cuando carga
+    setLoading(false);
   }, []);
 
   return (
@@ -52,7 +50,6 @@ export default function RootLayout({
         <Providers>
           <SessionProvider>
             <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-              {/* Loader solo mientras carga */}
               {loading ? <Loader /> : <Custom>{children}</Custom>}
               <Toaster position="top-center" reverseOrder={false} />
             </ThemeProvider>
