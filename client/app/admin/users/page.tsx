0247e99600c157900b80f7cd37232e051536f26c
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-03-19 09:40:40 -0500

    Agregar componente AllUsers y actualizar la página de administración de usuarios

--- client/app/admin/users/page.tsx
+++ client/app/admin/users/page.tsx
@@ -2,6 +2,7 @@
 import Heading from "../../components/utils/Heading";
 import AdminSidebar from "../../components/Admin/sidebar/AdminSidebar";
 import DashboardHeader from "../../components/Admin/DashboardHeader";
+import AllUsers from "../../components/Admin/users/AllUsers";
 import React from "react";
 
 type Props = {};
@@ -9,13 +10,14 @@ type Props = {};
 const page = (props: Props) => {
   return (
     <>
-      <Heading title="Ver cursos - SaberX" />
+      <Heading title="Ver usuarios - SaberX" />
       <div className="flex">
         <div className="1500px:w-[16%] w-1/5">
           <AdminSidebar />
         </div>
         <div className="w-[95%]">
           <DashboardHeader />
+          <AllUsers />
         </div>
       </div>
     </>
---
+++ client/app/components/Admin/users/AllUsers.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const AllUsers = (props: Props) => {
+  return <div className="text-black">AllUsers</div>;
+};
+
+export default AllUsers;
