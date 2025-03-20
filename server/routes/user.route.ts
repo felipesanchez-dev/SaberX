e7c420b2de4abf4f0d0942b0bc2d1fa697c1a374
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-03-20 15:55:14 -0500

    Eliminar la verificación del token de actualización en la ruta de obtención de información del usuario

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -28,7 +28,7 @@ userRouter.get("/logout", isAuthenticated, logoutUser);
 
 userRouter.get("/refresh", updateAccessToken);
 
-userRouter.get("/me", updateAccessToken, isAuthenticated, getUserInfo);
+userRouter.get("/me", isAuthenticated, getUserInfo);
 
 userRouter.post("/social-auth", socialAuth);
 
